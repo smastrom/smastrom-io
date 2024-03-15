d4439568b2ee327a272eb49b129afa39d2f8f27c
smastrom <smastrom@proton.me>
2024-03-15 19:50:21 +0100

    add api route to get packages

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,43 @@ export function getLinkProps(url: URL, href: string) {
       ...(url.pathname === href ? { 'data-active': '' } : {}),
    }
 }
+
+export const isFirefox = () => navigator.userAgent.indexOf('Firefox') !== -1
+export const isSafari = () => navigator.userAgent.indexOf('Safari') !== -1
+
+export function prefetch() {
+   navLinks.forEach((l) => {
+      if (window.location.pathname !== l.href) astroPrefetch(l.href, { with: 'fetch' })
+   })
+}
+
+export function formatNumber(num: number) {
+   if (num >= 1000000) return `${(num / 1000000).toFixed(1)}M`
+   if (num >= 1000) return `${(num / 1000).toFixed(1)}k`
+
+   return num.toString()
+}
+
+export function capitalizeAll(str: string) {
+   return str
+      .split('-')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ')
+}
+
+export function formatDate(date: string) {
+   return new Date(date)
+      .toLocaleDateString('en-US', {
+         month: 'short',
+         day: 'numeric',
+         year: 'numeric',
+      })
+      .toLowerCase()
+}
+
+const WRANGLER_LOCAL_URL = 'http://127.0.0.1:8788'
+
+export function getBaseUrl() {
+   if (import.meta.env.DEV) return 'http://localhost:4321'
+   return import.meta.env.CF_PAGES_URL || WRANGLER_LOCAL_URL
+}
---
+++ src/pages/api/packages.ts
@@ -0,0 +1,87 @@
+import type { APIRoute } from 'astro'
+
+import { capitalizeAll } from '@/lib/utils'
+
+export interface Package {
+   title: string
+   downloads: number
+   stargazers_count: number
+   description: string
+   created_at: string
+   demo_url: string | null
+   npm_url: string
+   github_url: string
+}
+
+const removeScope = (pkg: string) => (pkg.startsWith('@') ? pkg.split('/')[1] : pkg)
+
+export const GET: APIRoute = async ({ request }) => {
+   try {
+      const url = new URL(request.url)
+
+      const packages = new URLSearchParams(url.search).get('packages').split(',')
+
+      const result = packages.reduce(
+         (acc, pkg, i) => {
+            acc[i] = {
+               title: capitalizeAll(removeScope(pkg)).replaceAll('-', ' '),
+               npm_url: `https://www.npmjs.com/package/${pkg}`,
+            } as Package
+            return acc
+         },
+         {} as Record<string, Package>
+      )
+
+      const requests = [
+         ...packages.map(async (pkg, i) => {
+            const res = await fetch(
+               `https://api.npmjs.org/downloads/point/2000-01-01:2100-12-31/${pkg}`
+            )
+
+            if (!res.ok) throw new Error(await res.text())
+
+            const data = await res.json()
+            result[i].downloads = data.downloads
+         }),
+
+         ...packages.map(async (pkg, i) => {
+            const res = await fetch(`https://api.github.com/repos/smastrom/${removeScope(pkg)}`, {
+               headers: {
+                  Accept: 'application/vnd.github+json',
+                  Authorization: `Bearer ${import.meta.env.GITHUB_TOKEN}`,
+                  'User-Agent': 'smastrom',
+               },
+            })
+
+            if (!res.ok) throw new Error(await res.text())
+
+            const data = await res.json()
+            Object.assign(result[i], {
+               demo_url: data.homepage || null,
+               created_at: data.created_at,
+               description: data.description.replace(/:[^:]+: /, ''),
+               stargazers_count: data.stargazers_count,
+               github_url: data.html_url,
+            })
+         }),
+      ]
+
+      await Promise.all(requests)
+
+      return new Response(JSON.stringify(Object.values(result)), {
+         status: 200,
+         headers: {
+            'Content-Type': 'application/json',
+         },
+      })
+   } catch (err) {
+      console.error(err)
+
+      return new Response(JSON.stringify({ error: err.message }), {
+         status: 500,
+         headers: {
+            'Content-Type': 'application/json',
+         },
+      })
+   }
+}

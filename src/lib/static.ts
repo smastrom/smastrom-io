3067a71dfda5680adef5284922025c60feff9e5d
smastrom <smastrom@proton.me>
2024-03-19 12:33:08 +0100

    move icons to their folder

--- src/lib/static.ts
+++ src/lib/static.ts
@@ -1,6 +1,6 @@
-import IconFingerprint from '@/components/IconFingerprint.astro'
-import IconGit from '@/components/IconGit.astro'
-import IconHandshake from '@/components/IconHandshake.astro'
+import IconFingerprint from '@/components/icons/IconFingerprint.astro'
+import IconGit from '@/components/icons/IconGit.astro'
+import IconHandshake from '@/components/icons/IconHandshake.astro'
 
 export const navLinks = [
    { caption: 'About Me', href: '/', Icon: IconFingerprint },

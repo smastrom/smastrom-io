2d44e13fd6f9662e3cbc3074bfc0c09d7c920872
smastrom <smastrom@proton.me>
2024-04-19 21:43:43 +0200

    prepare for blog: move design syst to vars, custom media, global classes, cleanup

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,21 @@
+import globalData from '@csstools/postcss-global-data'
 import mixins from 'postcss-mixins'
+import customMedia from 'postcss-custom-media'
 import nesting from 'postcss-nesting'
 import responsiveType from 'postcss-responsive-type'
 import hoverMediaFeature from 'postcss-hover-media-feature'
 import autoprefixer from 'autoprefixer'
 
 export default {
-   plugins: [mixins(), autoprefixer(), nesting(), responsiveType(), hoverMediaFeature()],
+   plugins: [
+      globalData({
+         files: ['./src/styles/breakpoints.css'],
+      }),
+      customMedia(),
+      mixins(),
+      autoprefixer(),
+      nesting(),
+      responsiveType(),
+      hoverMediaFeature(),
+   ],
 }

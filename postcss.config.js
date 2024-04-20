9928380dc172bdda563f4a13fc5871069b852c68
smastrom <smastrom@proton.me>
2024-04-20 23:50:24 +0200

    make all design tokens reusable, replace responsive-type with native css clamp

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ import globalData from '@csstools/postcss-global-data'
 import mixins from 'postcss-mixins'
 import customMedia from 'postcss-custom-media'
 import nesting from 'postcss-nesting'
-import responsiveType from 'postcss-responsive-type'
 import hoverMediaFeature from 'postcss-hover-media-feature'
 import autoprefixer from 'autoprefixer'
 
@@ -11,11 +10,10 @@ export default {
       globalData({
          files: ['./src/styles/breakpoints.css'],
       }),
-      customMedia(),
       mixins(),
-      autoprefixer(),
       nesting(),
-      responsiveType(),
       hoverMediaFeature(),
+      customMedia(),
+      autoprefixer(),
    ],
 }

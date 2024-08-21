da17242eaa461256c2a095d94fdab38837eef343
smastrom <smastrom@proton.me>
2024-08-21 13:52:18 +0200

    round stats format if no decimal, rename project cards commit caption

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,8 +8,8 @@ export function getLinkProps(url: URL, href: string) {
 }
 
 export function formatNumber(num: number) {
-   if (num >= 1000000) return `${(num / 1000000).toFixed(1)}M`
-   if (num >= 1000) return `${(num / 1000).toFixed(1)}k`
+   if (num >= 1000000) return `${(num / 1000000).toFixed(1)}M`.replace('.0', '')
+   if (num >= 1000) return `${(num / 1000).toFixed(1)}k`.replace('.0', '')
 
    return num.toString()
 }

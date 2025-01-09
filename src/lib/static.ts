3f267efb6c0ed65c3ca23c3430a194cfd9c782f6
smastrom <smastrom@proton.me>
2025-01-09 12:29:14 +0100

    rename professional-work to services

--- src/lib/static.ts
+++ src/lib/static.ts
@@ -5,7 +5,7 @@ import IconHandshake from '@/components/icons/IconHandshake.astro'
 export const navLinks = [
    { caption: 'About Me', href: '/', Icon: IconFingerprint },
    { caption: 'Open Source', href: '/open-source', Icon: IconGit },
-   { caption: 'Professional Work', href: '/professional-work', Icon: IconHandshake },
+   { caption: 'Services', href: '/services', Icon: IconHandshake },
 ]
 
 export const packages = [

d9b6ceedf12e81377a1a9a74c8047dd69c6f34fe
smastrom <smastrom@proton.me>
2026-01-11 14:15:43 +0100

    some refactor renaming

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,10 @@
-export const NAME = 'Simone M.'
+import type { PackageSlug } from '@/lib/types'
 
-export const EMAIL = 'hello@smastrom.io'
+export const AUTHOR_NAME = 'Simone M.'
+
+export const AUTHOR_EMAIL = 'hello@smastrom.io'
+
+export const AUTHOR_COUNTRY = 'Italy'
 
 export const SITE_URL = 'https://smastrom.io'
 
@@ -8,3 +12,23 @@ export const LINKS = {
    github: 'https://github.com/smastrom',
    discord: 'https://discordapp.com/users/1174395637660401695',
 }
+
+export const NPM_USERNAME = 'smastrom'
+
+export const TOP_PACKAGES = [
+   'notivue',
+   'vue-collapsed',
+   'vue-use-active-scroll',
+   '@smastrom/react-rating',
+] as PackageSlug[]
+
+export const TOPICS = [
+   'Vite',
+   'Vue.js',
+   'Nuxt',
+   'Astro',
+   'Cloudflare',
+   'Supabase',
+   'Directus',
+   'Storyblok',
+]
